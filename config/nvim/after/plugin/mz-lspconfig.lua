e85e1e00f5c907ad2fdb09208c1be8eab88e1cad
Andreas Glashauser <andreas.glashauser@csp-sw.de>
2025-08-07 21:15:21 +0200

    Added: ts/js and react support

--- config/nvim/after/plugin/mz-lspconfig.lua
+++ config/nvim/after/plugin/mz-lspconfig.lua
@@ -9,6 +9,7 @@ local on_attach = function(client, bufnr)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
     vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, bufopts)
+    vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, bufopts)
 
     -- Show diagnostics on cursor hold
     vim.api.nvim_create_autocmd("CursorHold", {
@@ -17,6 +18,16 @@ local on_attach = function(client, bufnr)
             vim.diagnostic.open_float(nil, { focus = false })
         end
     })
+    
+    -- Format on save
+    if client.server_capabilities.documentFormattingProvider then
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            buffer = bufnr,
+            callback = function()
+                vim.lsp.buf.format({ timeout_ms = 2000 })
+            end
+        })
+    end
 end
 
 
@@ -49,3 +60,22 @@ vim.api.nvim_create_autocmd("LspAttach", {
   end,
   desc = 'LSP: Disable hover capability from Ruff',
 })
+
+
+lspconfig.eslint.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+    -- Allow using globally installed eslint when a local one is missing
+    cmd = { "bash", "-lc", "NODE_PATH=$(npm root -g) vscode-eslint-language-server --stdio" },
+    settings = {
+        workingDirectories = {
+            { mode = "auto" }
+        }
+    }
+}
+
+lspconfig.ts_ls.setup {
+    capabilities = capabilities,
+    on_attach = on_attach,
+}
+
---
+++ test.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const   BadComponent=({name}:{name:string})=>{
+const unused_variable = 1;
+return<div>Hello {name}</div>
+}
+
+export default BadComponent;
