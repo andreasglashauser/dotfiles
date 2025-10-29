e29f14364a6dda422a9fe41bb5d4a60c2b443b1b
Andreas Glashauser <ag@andreasglashauser.com>
2025-10-29 17:34:46 +0100

    feat: added llamacpp config

---
+++ config/nvim/after/plugin/llamacpp.lua
@@ -0,0 +1,25 @@
+vim.g.llama_config = {
+  endpoint = 'http://127.0.0.1:8080/infill',
+  api_key = '',
+  model = '',
+  n_prefix = 256,
+  n_suffix = 64,
+  n_predict = 128,
+  stop_strings = {},
+  t_max_prompt_ms = 500,
+  t_max_predict_ms = 500,
+  show_info = 2,
+  auto_fim = true,
+  max_line_suffix = 8,
+  max_cache_keys = 250,
+  ring_n_chunks = 16,
+  ring_chunk_size = 64,
+  ring_scope = 1024,
+  ring_update_ms = 1000,
+  keymap_trigger = '<C-F>',
+  keymap_accept_full = '<Tab>',
+  keymap_accept_line = '<S-Tab>',
+  keymap_accept_word = '<C-B>',
+  enable_at_startup = true
+}
+
